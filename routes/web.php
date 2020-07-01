ff620efdc93f29dab16bb080e999e0a57d0abb98
Irsyad M <irsyad.m2015@gmail.com>
2020-07-02 01:00:25 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,20 @@
     return view('welcome');
 }); */
 
-Route::get('/', 'HomeController@View');
+//Route::get('/', 'HomeController@View');
+
+Route::get('/', function(){
+    return view('tables.table1');
+});
+
+Route::get('/data-tables', function(){
+    return view('tables.data');
+});
 
 Route::get('/register', 'AuthController@RegisView');
 
 Route::post('/welcome', 'AuthController@WelcomePost');
+
+Route::get('/master', function(){
+    return view('adminlte.master');
+});
